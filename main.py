ee04fb88fbcee8308ceec8940084cca976bc72ff
cesarlmt27 <72035439+cesarlmt27@users.noreply.github.com>
2022-09-10 23:39:01 -0300

    Nuevos cambios

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from read import *
 
 #Solicitar sueldo bruto.
-print("Ingrese sueldo bruto: ")
-sueldo_bruto = float(input())
+#print("Ingrese sueldo bruto: ")
+sueldo_bruto = 5363550.01 #float(input())
 
 #Solicitar administradora AFP.
-print("Seleccione administradora AFP: ")
-adm_afp = input()
+#print("Seleccione administradora AFP: ")
+adm_afp = 'AFP Capital' #input()
 
 
 comision_afp = float(administradoras_afp[adm_afp])  #Declarar valor de la comisión de la administradora AFP.
@@ -19,4 +19,19 @@ def calcular_sueldo_imponible(sueldo_bruto, dp_fijos, comision_afp):
 
 
 sueldo_imponible = calcular_sueldo_imponible(sueldo_bruto, dp_fijos, comision_afp)
-print(sueldo_imponible)
+
+
+for i in tabla_si:  #Recorrer cada lista de la lista "tabla_si"
+    if(sueldo_imponible > float(i[0]) and sueldo_imponible < float(i[1])):  #Verificar el rango donde se encuentra el sueldo imponible obtenido.
+        factor = float(i[2])       #Declarar el factor del rango donde se encuentra el sueldo imponible.
+        descuento = float(i[3])    #Declarar el descuento del rango donde se encuentra el sueldo imponible.
+
+
+def calcular_sueldo_liquido(sueldo_imponible, factor, descuento):
+    impuestos_legales = sueldo_imponible * factor - descuento
+    sueldo_liquido = sueldo_imponible - impuestos_legales
+    return round(sueldo_liquido, 2)
+
+sueldo_liquido = calcular_sueldo_liquido(sueldo_imponible, factor, descuento)
+
+print(sueldo_liquido)
--- read.py
+++ read.py
@@ -14,3 +14,13 @@ with open('csv/adm_afp.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             administradoras_afp[row[0]] = row[1]
+
+
+#Leer y almacenar las filas de "tabla_si.csv" en una lista.
+tabla_si = []
+with open('csv/tabla_si.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            tabla_si.append(row)
+
+tabla_si.pop(0) #Eliminar primer elemento de la lista.
